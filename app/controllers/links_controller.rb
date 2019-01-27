fad64e1855d5d1084831d9bfc0838eaf127cd04c
John Henderson <johnhenderson@fastmail.fm>
2019-01-27 14:47:41 +0000

    New link form now offers radio buttons to choose which heading link will go under

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -4,7 +4,18 @@ class LinksController < ApplicationController
     @links = Link.all
   end
 
-  def new # Action for creating new link
+  def new # Action for preparing the storage of a new link
     @link = Link.new
+  end
+
+  def create # Action connected to 'Create' button on new link page
+    Link.create(link_params) # Sends the data from 'link_params' to the database
+    redirect_to root_path # After new link is stored, redirect user to index page
+  end
+
+  private
+
+  def link_params # Pulls 'heading_id', 'description' and 'URL' from the link form
+    params.require(:link).permit(:heading_id, :description, :URL)
   end
 end
